af0139567feea66a37682c101c525fa95a8fc05e
Giles Edkins <edkins@gmail.com>
2017-08-06 17:59:17 -0400

    Fix some things which were confusing me

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-thing = function(){
-  var str, last, avgUp, avgDown;
-  str = function(it){
-    return JSON.stringify(it);
-  };
-  last = 0;
-  avgUp = 0;
-  avgDown = 0;
-  $('input').on('keypress', function(arg$){
-    var key, which, timeStamp, dif, height;
-    key = arg$.key, which = arg$.which, timeStamp = arg$.timeStamp;
-    dif = timeStamp - last;
-    last = timeStamp;
-    avgDown = (3 * avgDown + Math.sqrt(dif)) / 4;
-    height = Math.min(10, avgDown - 8);
-    console.log('down', key, Math.round(avgDown), Math.round(Math.sqrt(dif)));
-    $('#output').append($('<span>').addClass("f" + Math.round(Math.min(9, 11 - height))).text(String.fromCharCode(which)));
-    return $('#outgraph').append($('<div class=foo>').append($('<div class=pad>').height((10 - height) + "rem")).append($('<div class=bar>').height(height + "rem")));
+function weight_class(weight)
+{
+	return "f" + Math.round(Math.min(9, weight));
+}
+
+function adjust(dif)
+{
+	return Math.pow(dif / 4, 0.5);
+}
+
+function letter(weight, charcode)
+{
+	return $('<span>')
+	    	.addClass(weight_class(weight))
+		.text(String.fromCharCode(charcode));
+}
+
+var state = {
+	last: 0
+};
+
+var thing = function(){
+  $('input').on('keypress', function(event){
+	var key, which, timeStamp, dif, weight;
+	var key = event.key, which = event.which;
+	var timeStamp = event.timeStamp;
+	var dif = timeStamp - state.last;
+	state.last = timeStamp;
+	var avgDown = adjust(dif);
+	var weight = 11 - Math.max(0,Math.min(10, avgDown));
+	$('#output').append(letter(weight, which));
+
+	$('#outgraph')
+		.append($('<div class=foo>')
+		.append($('<div class=pad>')
+		.height((10 - weight) + "rem"))
+		.append($('<div class=bar>')
+		.height(weight + "rem")));
   });
 };
 
